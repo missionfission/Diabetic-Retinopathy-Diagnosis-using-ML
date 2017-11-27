833dd465bd15ec938d3cc2ba0fa73624e4c1b2b1
Mayank Mishra <mayank31398@gmail.com>
2017-11-27 12:55:21 +0530

    changes made

--- Separate.m
+++ Separate.m
@@ -7,17 +7,17 @@ for i=1:size(x,1)
 end
 r_count=size(x,1)-l_count;
 
-L=cell(l_count,1);
-R=cell(r_count,1);
+L=zeros(l_count,8);
+R=zeros(r_count,8);
 l_count=0;
 r_count=0;
 
 for i=1:size(x,1)
     if endsWith(x{i}.filename,"L")
         l_count=l_count+1;
-        L(l_count)=x(i);
+        L(l_count,:)=x{i}.medians;
     else
         r_count=r_count+1;
-        R(r_count)=x(i);
+        R(r_count,:)=x{i}.medians;
     end
 end
